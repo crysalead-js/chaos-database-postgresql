19d831d4596d68e46585a4f7aa8ad3737f0a9af2
Simon JAILLET <sjaillet@gmail.com>
2016-07-02 04:13:05 +0200

    Update dependencies.

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ class PostgreSql extends Database {
         if (typeof type === 'function') {
           type = type(states.name);
         }
-        return this.format('datasource', type, value);
+        return this.convert('datasource', type, value);
       }.bind(this)
     });
   }
